d56bac3420479e36957e0e7dbef2f2b3fd40e784
gaoshanshan <gaoshanshan835@lu.com>
2016-05-11 16:59:46 +0800

    160511

--- src/tree/FlatenBinaryTreeToLinkedList.java
+++ src/tree/FlatenBinaryTreeToLinkedList.java
@@ -48,16 +48,16 @@ class FlatenBinaryTreeToLinkedList {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         TreeNode n1 = new TreeNode(2);
-        TreeNode n2 = new TreeNode(3);
-        TreeNode n3 = new TreeNode(4);
-        TreeNode n4 = new TreeNode(5);
+        TreeNode n2 = new TreeNode(5);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
         TreeNode n5 = new TreeNode(6);
         root.left = n1;
         root.right = n2;
         n1.left = n3;
         n1.right = n4;
-        n2.left = n5;
-        new FlatenBinaryTreeToLinkedList().flattenB(root);
+        n2.right = n5;
+        new FlatenBinaryTreeToLinkedList().flatten(root);
         System.out.println(levelOrder(root));
     }
 
