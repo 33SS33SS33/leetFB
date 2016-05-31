8c415a7acb48b6972e2ca80c72f992c5760d9730
shanshan <shanshan@shanshandeMacBook-Pro.local>
2016-05-31 22:37:45 +0800

    160531

--- src/hard/SmallestRectangleEnclosingBlackPixels.java
+++ src/hard/SmallestRectangleEnclosingBlackPixels.java
@@ -5,12 +5,10 @@ package hard;
  */
 
 /**
- * "肯定可以用dfs来做  但是效率比较慢 比较快的是可以用binary search
- * 这题最关键的前提是 只有一个黑色的区域 所以我们可以用binary search 分别找出来这个区域的轮廓  就是最上面的一个还有1的行
- * 最下面的含有1的行 最左面含有1的列 和最右边含有1的列"
  * "An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel.
  * The black pixels are connected, i.e., there is only one black region. Pixels are connected horizontally and vertically.
- * Given the location (x, y) of one of the black pixels, return the area of the smallest (axis-aligned) rectangle that encloses all black pixels.
+ * Given the location (x, y) of one of the black pixels,
+ * return the area of the smallest (axis-aligned) rectangle that encloses all black pixels.
  * For example, given the following image:
  * [
  * "0010",
@@ -19,8 +17,16 @@ package hard;
  * ]
  * and x = 0, y = 2,
  * Return 6."
+ *
+ * "肯定可以用dfs来做  但是效率比较慢 比较快的是可以用binary search
+ * 这题最关键的前提是 只有一个黑色的区域 所以我们可以用binary search 分别找出来这个区域的轮廓  就是最上面的一个还有1的行
+ * 最下面的含有1的行 最左面含有1的列 和最右边含有1的列"
  */
 public class SmallestRectangleEnclosingBlackPixels {
+    public static void main(String[] args) {
+        char[][] iImage = {{'0','0','1','0'},{'0','1','1','0'},{'0','1','0','0'}};
+        System.out.println(new SmallestRectangleEnclosingBlackPixels().minArea(iImage, 0,2));
+    }
 
     private char[][] image;
 
