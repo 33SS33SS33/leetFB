674341a620857c7d40c15dd601796f3ac1bcdae1
gaoshanshan <gaoshanshan835@lu.com>
2016-06-01 13:21:08 +0800

    20160601

--- src/hard/StrobogrammaticNumber3.java
+++ src/hard/StrobogrammaticNumber3.java
@@ -2,26 +2,20 @@ package hard;
 
 /**
  * Created by GAOSHANSHAN835 on 2016/5/9.
- * This is a follow up of Shortest Word Distance. The only difference is now word1 could be the same as word2.
- * Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
- * word1 and word2 may be the same and they represent two individual words in the list.
- * For example,
- * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
- * Given word1 = “makes”, word2 = “coding”, return 1.
- * Given word1 = "makes", word2 = "makes", return 3.
- * Note:
- * You may assume word1 and word2 are both in the list.
+ A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+ Write a function to count the total strobogrammatic numbers that exist in the range of low <= num <= high.
+ For example,
+ Given low = "50", high = "100", return 3. Because 69, 88, and 96 are three strobogrammatic numbers.
  */
 
 /**
  * 主要思路是比如求50-100范围的时候 先计算小于50的个数  再计算小于100的个数 然后用小于100的个数减去小于50的个数
- * 然后单独计算小于i的个数的时候 是直接用数学方法算出来  i的位数-1 的个数  然后再算出来i的位数一共有多少个 然后从这些里面找出来小于i的 然后把两个个数加起来
+ * 然后单独计算小于i的个数的时候 是直接用数学方法算出来  i的位数-1 的个数
+ * 然后再算出来i的位数一共有多少个 然后从这些里面找出来小于i的 然后把两个个数加起来
  */
 public class StrobogrammaticNumber3 {
     public static void main(String[] args) {
-        String low="makes";
-        String high="coding";
-        System.out.println(new StrobogrammaticNumber3().strobogrammaticInRange(low,high));
+        System.out.println(new StrobogrammaticNumber3().strobogrammaticInRange("50","100"));
     }
 
     char[][] pairs = { { '0', '0' }, { '1', '1' }, { '6', '9' }, { '8', '8' }, { '9', '6' } };
--- src/medium/StrobogrammaticNumber2.java
+++ src/medium/StrobogrammaticNumber2.java
@@ -2,17 +2,45 @@ package medium;
 
 /**
  * Created by GAOSHANSHAN835 on 2016/5/9.
- * <p/>
- * This is a follow up of Shortest Word Distance. The only difference is now you are given the list of words and your method will be called repeatedly many times with different parameters. How would you optimize it?
- * Design a class which receives a list of words in the constructor, and implements a method that takes two words word1 and word2 and return the shortest distance between these two words in the list.
+ * A strobogrammatic number is a number that looks the same when rotated 180 degrees
+ * (looked at upside down).
+ * Find all strobogrammatic numbers that are of length = n.
  * For example,
- * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
- * Given word1 = “coding”, word2 = “practice”, return 3.
- * Given word1 = "makes", word2 = "coding", return 1.
+ * Given n = 2, return ["11","69","88","96"].
  */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 用dfs就行 分奇数和偶数两种情况 先插入最中间的会比较方便(因为每次插入两边的代码好写)
  */
 public class StrobogrammaticNumber2 {
+    public static void main(String[] args) {
+        System.out.println(new StrobogrammaticNumber2().findStrobogrammatic(2));
+    }
+
+    public List<String> findStrobogrammatic(int n) {
+        return helper(n, n);
+    }
+
+    List<String> helper(int n, int m) {
+        if (n == 0)
+            return new ArrayList<String>(Arrays.asList(""));
+        if (n == 1)
+            return new ArrayList<String>(Arrays.asList("0", "1", "8"));
+        List<String> list = helper(n - 2, m);
+        List<String> res = new ArrayList<String>();
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if (n != m)
+                res.add("0" + s + "0");
+            res.add("1" + s + "1");
+            res.add("6" + s + "9");
+            res.add("8" + s + "8");
+            res.add("9" + s + "6");
+        }
+        return res;
+    }
 }
