82d0bf99c6348fcb797237da282acec210941dac
gaoshanshan <gaoshanshan835@lu.com>
2016-06-06 14:21:44 +0800

    160606

--- src/easy/MoveZeroes.java
+++ src/easy/MoveZeroes.java
@@ -44,20 +44,6 @@ public class MoveZeroes {
         }
     }
 
-    void shiftLeft(int[] nums, int p, final int c) {
-        if (c <= 0)
-            return;
-        while (p < nums.length) {
-            nums[p - c] = nums[p];
-            if (nums[p] == 0)
-                break;
-            p++;
-        }
-        for (int i = p - c; i < p; i++) {
-            nums[i] = 0;
-        }
-    }
-
     public void moveZeroes(int[] nums) {
         int i = nums.length - 1;
         // trim tailing zero
@@ -79,4 +65,19 @@ public class MoveZeroes {
         }
         shiftLeft(nums, s, s);
     }
+
+    void shiftLeft(int[] nums, int p, final int c) {
+        if (c <= 0)
+            return;
+        while (p < nums.length) {
+            nums[p - c] = nums[p];
+            if (nums[p] == 0)
+                break;
+            p++;
+        }
+        for (int i = p - c; i < p; i++) {
+            nums[i] = 0;
+        }
+    }
+
 }
