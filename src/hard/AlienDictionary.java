24e527bab723d2d8a3e2b57cd6bfad00e6c11cd3
gaoshanshan <gaoshanshan835@lu.com>
2016-06-07 16:56:20 +0800

    160607

--- src/hard/AlienDictionary.java
+++ src/hard/AlienDictionary.java
@@ -23,23 +23,6 @@ import java.util.Queue;
  * If the order is invalid, return an empty string.
  * There may be multiple valid order of letters, return any one of them is fine.
  */
-/**There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of words from the dictionary,
- *  where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
- For example,
- Given the following words in dictionary,
- [
- "wrt",
- "wrf",
- "er",
- "ett",
- "rftt"
- ]
- The correct order is: "wertf".
- Note:
- You may assume all letters are in lowercase.
- If the order is invalid, return an empty string.
- There may be multiple valid order of letters, return any one of them is fine.
- */
 
 /**
  * 这道题分两步  第一步是通过字典里的单词建成有向图, 这道题的真正意思是 每一个word都是一个单词 单词是按照字母顺序表示的 所以找到顺序就是找到上下单词里的第一个不同的字母
@@ -51,21 +34,22 @@ public class AlienDictionary {
         String[] words = { "wrt", "wrf", "er", "ett", "rftt" };
         System.out.println(new AlienDictionary().alienOrder(words));
     }
-/*    private void filterBizStepData(final MonitorItemDTO item) {
-        final List<AlarmItem> alarmItems = bizStepFilter.filter(item);
 
-        alarmItemDAO.batchInsert(alarmItems);
+    /*    private void filterBizStepData(final MonitorItemDTO item) {
+            final List<AlarmItem> alarmItems = bizStepFilter.filter(item);
 
-        Logger.info(this,"start thread to notify alarm ");
-        threadPoolTaskExecutor.execute(new Runnable() {
-            public void run() {
-                Logger.info(this, "start to notify alarm ");
-                alarmNotifyRemoteService.alarmNotify(alarmItems);
-                Logger.info(this,"end to notify alarm ");
-            }
-        });
+            alarmItemDAO.batchInsert(alarmItems);
 
-    }*/
+            Logger.info(this,"start thread to notify alarm ");
+            threadPoolTaskExecutor.execute(new Runnable() {
+                public void run() {
+                    Logger.info(this, "start to notify alarm ");
+                    alarmNotifyRemoteService.alarmNotify(alarmItems);
+                    Logger.info(this,"end to notify alarm ");
+                }
+            });
+
+        }*/
     public String alienOrder(String[] words) {
         Node[] node = new Node[26];
         boolean[] happen = new boolean[26];
