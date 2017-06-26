d674d5dc3841e89f309f00a0f13f4265de21ba6a
shanshan <12210240086@fudan.edu.cn>
2017-06-26 19:47:15 +0800

    170626

--- src/aFB/DiameterofBinaryTree.java
+++ src/aFB/DiameterofBinaryTree.java
@@ -5,4 +5,21 @@ package aFB;
  */
 public class DiameterofBinaryTree {
     //https://discuss.leetcode.com/topic/83456/java-solution-maxdepth
+    int max = 0;
+
+    public int diameterOfBinaryTree(LowestCommonAncestorofaBST.TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    private int maxDepth(LowestCommonAncestorofaBST.TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        max = Math.max(max, left + right);
+
+        return Math.max(left, right) + 1;
+    }
 }
