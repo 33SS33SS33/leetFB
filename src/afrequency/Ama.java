beb8f7a8cad749a97d80ec316fc210a854607405
kgao <krystal.gao@blackboard.com>
2017-07-03 15:11:15 +0800

    710703

--- src/afrequency/Ama.java
+++ src/afrequency/Ama.java
@@ -4,76 +4,79 @@ package afrequency;
  * Created by shanshan on 16/5/22.
  */
 public class Ama {
-    /** 42道  67.
+    /** 42道
+     * 67
+     * 68. 170703
      #
      Title  Acceptance  Difficulty  Frequency
 
-     146	LRU Cache	17.2%	Hard
-     1	    Two Sum	33.2%	Easy
-     200	Number of Islands	33.8%	Medium
-     297	Serialize and Deserialize Binary Tree	32.8%	Hard
-     20	    Valid Parentheses	33.1%	Easy
+     146	LRU Cache	17.3%	Hard
+     1	    Two Sum	33.7%	Easy
+     200	Number of Islands	34.1%	Medium
+     297	Serialize and Deserialize Binary Tree	32.9%	Hard
+     20	    Valid Parentheses	33.2%	Easy
      138	Copy List with Random Pointer	26.4%	Medium
-     48	    Rotate Image	38.1%	Medium
-     460	LFU Cache	22.9%	Hard
+     48	    Rotate Image	38.2%	Medium
      236	Lowest Common Ancestor of a Binary Tree	29.7%	Medium
-     438	Find All Anagrams in a String	33.4%	Easy
-     238	Product of Array Except Self	48.4%	Medium
+     460	LFU Cache	23.2%	Hard
+     438	Find All Anagrams in a String	33.5%	Easy
+     238	Product of Array Except Self	48.7%	Medium
      5	    Longest Palindromic Substring	25.1%	Medium
-     121	Best Time to Buy and Sell Stock	40.5%	Easy
-     239	Sliding Window Maximum	32.5%	Hard
-     17	    Letter Combinations of a Phone Number	33.9%	Medium
-     206	Reverse Linked List	44.9%	Easy
+     121	Best Time to Buy and Sell Stock	40.8%	Easy
+     239	Sliding Window Maximum	32.6%	Hard
+     17	    Letter Combinations of a Phone Number	34.1%	Medium
+     206	Reverse Linked List	45.1%	Easy
      380	Insert Delete GetRandom O(1)	38.9%	Medium
+     235	Lowest Common Ancestor of a Binary Search Tree	38.8%	Easy
      387	First Unique Character in a String	46.4%	Easy
-     235	Lowest Common Ancestor of a Binary Search Tree	38.7%	Easy
      127	Word Ladder	19.3%	Medium
-     240	Search a 2D Matrix II	38.2%	Medium
-     42	    Trapping Rain Water	36.3%	Hard
-     155	Min Stack	27.9%	Easy
-     49	    Group Anagrams	33.6%	Medium
-     160	Intersection of Two Linked Lists	30.4%	Easy
+     240	Search a 2D Matrix II	38.3%	Medium
+     42	    Trapping Rain Water	36.4%	Hard
+     155	Min Stack	28.1%	Easy
+     49	    Group Anagrams	33.9%	Medium
+     160	Intersection of Two Linked Lists	30.5%	Easy
      141	Linked List Cycle	35.4%	Easy
-     2	    Add Two Numbers	27.3%	Medium
-     242	Valid Anagram	45.9%	Easy
-     139	Word Break	29.3%	Medium
-     215	Kth Largest Element in an Array	38.7%	Medium
-     126	Word Ladder II	13.9%	Hard
-     234	Palindrome Linked List	32.3%	Easy
-     98	    Validate Binary Search Tree	23.0%	Medium
-     23	    Merge k Sorted Lists	26.8%	Hard
-     3	    Longest Substring Without Repeating Characters	24.1%	Medium
-     167	Two Sum II - Input array is sorted	47.2%	Easy
-     21	    Merge Two Sorted Lists	38.8%	Easy
-     449	Serialize and Deserialize BST	41.9%	Medium
-     15	    3Sum	21.5%	Medium
+     242	Valid Anagram	46.1%	Easy
+     2	    Add Two Numbers	27.5%	Medium
+     139	Word Break	29.5%	Medium
+     126	Word Ladder II	14.0%	Hard
+     215	Kth Largest Element in an Array	38.8%	Medium
+     234	Palindrome Linked List	32.5%	Easy
+     98	    Validate Binary Search Tree	23.1%	Medium
+     23	    Merge k Sorted Lists	26.9%	Hard
+     3	    Longest Substring Without Repeating Characters	24.2%	Medium
+     21	    Merge Two Sorted Lists	38.9%	Easy
+     167	Two Sum II - Input array is sorted	47.1%	Easy
+     449	Serialize and Deserialize BST	42.2%	Medium
+     15	    3Sum	21.6%	Medium
      186	Reverse Words in a String II 	27.6%	Medium
-     78	    Subsets	39.2%	Medium
-     102	Binary Tree Level Order Traversal	38.8%	Medium
-     73	    Set Matrix Zeroes	35.6%	Medium
-     89	    Gray Code	40.5%	Medium
+     78	    Subsets	39.6%	Medium
+     102	Binary Tree Level Order Traversal	39.1%	Medium
+     89	    Gray Code	40.7%	Medium
+     73	    Set Matrix Zeroes	35.7%	Medium
      8	    String to Integer (atoi)	13.9%	Medium
-     414	Third Maximum Number	27.7%	Easy
-     204	Count Primes	26.4%	Easy
-     355	Design Twitter	25.1%	Medium
-     199	Binary Tree Right Side View	40.1%	Medium
-     119	Pascal's Triangle II	36.2%	Easy
+     204	Count Primes	26.5%	Easy
+     414	Third Maximum Number	27.8%	Easy
+     355	Design Twitter	25.2%	Medium
+     199	Binary Tree Right Side View	40.3%	Medium
+     119	Pascal's Triangle II	36.4%	Easy
+     535	Encode and Decode TinyURL	74.1%	Medium
+     532	K-diff Pairs in an Array	28.0%	Easy
+     396	Rotate Function	32.1%	Medium
+     516	Longest Palindromic Subsequence	42.4%	Medium
+     451	Sort Characters By Frequency	50.7%	Medium
+     508	Most Frequent Subtree Sum	52.2%	Medium
+     538	Convert BST to Greater Tree	52.9%	Medium
+     606	Construct String from Binary Tree	52.1%	Easy
+     536	Construct Binary Tree from String 	40.0%	Medium
+     459	Repeated Substring Pattern	38.2%	Easy
+     545	Boundary of Binary Tree 	30.1%	Medium
+     553	Optimal Division	55.1%	Medium
      534	Design TinyURL	0.0%	Medium
-     508	Most Frequent Subtree Sum	51.9%	Medium
-     459	Repeated Substring Pattern	38.3%	Easy
-     579	Find Cumulative Salary of an Employee 	6.9%	Hard
-     451	Sort Characters By Frequency	50.8%	Medium
-     537	Complex Number Multiplication	65.2%	Medium
-     535	Encode and Decode TinyURL	73.8%	Medium
-     532	K-diff Pairs in an Array	27.9%	Easy
-     516	Longest Palindromic Subsequence	42.3%	Medium
-     538	Convert BST to Greater Tree	52.7%	Medium
-     545	Boundary of Binary Tree 	29.2%	Medium
-     536	Construct Binary Tree from String 	39.1%	Medium
-     517	Super Washing Machines	35.9%	Hard
-     553	Optimal Division	55.2%	Medium
-     529	Minesweeper	51.0%	Medium
-     396	Rotate Function	31.9%	Medium
-     606	Construct String from Binary Tree	55.1%	Easy
+     537	Complex Number Multiplication	64.6%	Medium
+     579	Find Cumulative Salary of an Employee 	12.2%	Hard
+     517	Super Washing Machines	36.6%	Hard
+     529	Minesweeper	50.4%	Medium
+     617	Merge Two Binary Trees	69.9%	Easy
      */
 }
