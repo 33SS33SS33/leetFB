49d6ebb9273346504be3bb11323b7fd88b3f9b65
kgao <krystal.gao@blackboard.com>
2017-07-04 16:48:57 +0800

    170704

--- src/sum/FourSum.java
+++ src/sum/FourSum.java
@@ -25,8 +25,6 @@ public class FourSum {
         int[] num = {1, 0, -1, 0, -2, 2};
         System.out.println(new FourSum().fourSum(num, 0).toString());
         System.out.println(new FourSum().fourSumA(num, 0).toString());
-        System.out.println(new FourSum().fourSumB(num, 0).toString());
-
     }
 
     /**
@@ -106,65 +104,6 @@ public class FourSum {
                         r--;
                     }
                 }
-            }
-        }
-        return res;
-    }
-
-    /**
-     * 总时间复杂度是O(n^3)
-     */
-    public ArrayList<ArrayList<Integer>> fourSumB(int[] num, int target) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        if (num == null || num.length == 0)
-            return res;
-        Arrays.sort(num);
-        for (int i = num.length - 1; i > 2; i--) {
-            if (i == num.length - 1 || num[i] != num[i + 1]) {
-                ArrayList<ArrayList<Integer>> curRes = threeSum(num, i - 1, target - num[i]);
-                for (int j = 0; j < curRes.size(); j++) {
-                    curRes.get(j).add(num[i]);
-                }
-                res.addAll(curRes);
-            }
-        }
-        return res;
-    }
-
-    private ArrayList<ArrayList<Integer>> threeSum(int[] num, int end, int target) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        for (int i = end; i > 1; i--) {
-            if (i == end || num[i] != num[i + 1]) {
-                ArrayList<ArrayList<Integer>> curRes = twoSum(num, i - 1, target - num[i]);
-                for (int j = 0; j < curRes.size(); j++) {
-                    curRes.get(j).add(num[i]);
-                }
-                res.addAll(curRes);
-            }
-        }
-        return res;
-    }
-
-    private ArrayList<ArrayList<Integer>> twoSum(int[] num, int end, int target) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        int l = 0;
-        int r = end;
-        while (l < r) {
-            if (num[l] + num[r] == target) {
-                ArrayList<Integer> item = new ArrayList<Integer>();
-                item.add(num[l]);
-                item.add(num[r]);
-                res.add(item);
-                l++;
-                r--;
-                while (l < r && num[l] == num[l - 1])
-                    l++;
-                while (l < r && num[r] == num[r + 1])
-                    r--;
-            } else if (num[l] + num[r] > target) {
-                r--;
-            } else {
-                l++;
             }
         }
         return res;
