2b987a76f86431709f5c4f103c7bb4796daabd1a
shanshan <12210240086@fudan.edu.cn>
2017-08-15 08:40:54 +0800

    170815

--- src/medium/DesignTicTacToe.java
+++ src/medium/DesignTicTacToe.java
@@ -35,17 +35,14 @@ public class DesignTicTacToe {
      */
     public int move(int row, int col, int player) {
         int toAdd = player == 1 ? 1 : -1;
-
         rows[row] += toAdd;
         cols[col] += toAdd;
         if (row == col) {
             diagonal += toAdd;
         }
-
         if (col == (cols.length - row - 1)) {
             antiDiagonal += toAdd;
         }
-
         int size = rows.length;
         if (Math.abs(rows[row]) == size ||
                 Math.abs(cols[col]) == size ||
@@ -53,7 +50,7 @@ public class DesignTicTacToe {
                 Math.abs(antiDiagonal) == size) {
             return player;
         }
-
         return 0;
     }
+
 }
